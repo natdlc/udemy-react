05afd5b88a007c9dbffcf44eb1c7459a841ea398
nat corpuz <nate.corpuz1@gmail.com>
2022-05-23 06:01:27 +0800

    basic styling intuition by me

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,9 @@
-import ExpenseItem from './components/ExpenseItem'
+import ExpenseItem from './components/ExpenseItem';
 	
 function App() {
   return (
 		<div>
 		  <h2>Let's get started!</h2>
-			<p>This is also visible</p>
 			<ExpenseItem/>
 		</div>
   );
--- my-app/src/components/ExpenseItem.js
+++ my-app/src/components/ExpenseItem.js
@@ -1,7 +1,15 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
-  return (
-    <h2>Expense Item</h2>
-  )
+	return (
+		<div className="expense-item">
+			<div>May 23rd 2022</div>
+			<div className="expense-item__description">
+				<h2>Breakfast</h2>
+				<div className="expense-item__price">300</div>
+			</div>
+		</div>
+	);
 };
 
 export default ExpenseItem;
